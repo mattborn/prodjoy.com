281d096d5c613f8475f6e83a77421d5c8af47f0b
Matt Born <mattborn@users.noreply.github.com>
2023-06-09 12:12:24 -0500

    Add gallery

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
-ScrollReveal().reveal('section *', {
+ScrollReveal().reveal('#intro *', {
   cleanup: true,
   distance: '20%',
   interval: 100,
   origin: 'bottom',
 })
+
+window.onscroll = () => {
+  document.getElementById('gallery').style.marginLeft = `-${window.pageYOffset / 2}px`
+}
